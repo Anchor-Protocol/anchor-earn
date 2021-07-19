4a55e9349cc77b9a28cc45b6d87d8fc073f9cd8e
frostornge <frostornge@gmail.com>
2021-07-19 18:08:50 +0900

    feat(facade): add ethereum contract interface

---
+++ src/facade/interface/ether/index.ts
@@ -0,0 +1,5 @@
+import * as IConversionPool from './IConversionPool.json';
+import * as IRouterV2 from './IRouterV2.json';
+import * as IERC20 from './IERC20.json';
+
+export { IConversionPool, IRouterV2, IERC20 };
