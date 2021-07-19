8620fbe40ab223e9b54417946ba84dc346f144a3
frostornge <frostornge@gmail.com>
2021-07-19 14:20:12 +0900

    refactor(types): move global types (chain / network)

--- src/facade/output.ts
+++ src/facade/output.ts
@@ -31,17 +31,3 @@ export enum STATUS {
   UNSUCCESSFUL = 'unsuccessful',
   IN_PROGRESS = 'in-progress',
 }
-
-export enum CHAINS {
-  TERRA = 'terra',
-  ETHER = 'ether',
-}
-
-export enum NETWORKS {
-  COLUMBUS_4,
-  TEQUILA_0004,
-  ETHER_MAINNET = 'homestead',
-  ETHER_ROPSTEN = 'ropsten',
-  // BSC_MAINNET = 'bnb',
-  // BSC_TESTNET = 'bnbt',
-}
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export enum CHAINS {
+  TERRA = 'terra',
+  ETHER = 'ether',
+}
+
+export enum NETWORKS {
+  COLUMBUS_4,
+  TEQUILA_0004,
+  ETHER_MAINNET = 'homestead',
+  ETHER_ROPSTEN = 'ropsten',
+  // BSC_MAINNET = 'bnb',
+  // BSC_TESTNET = 'bnbt',
+}
