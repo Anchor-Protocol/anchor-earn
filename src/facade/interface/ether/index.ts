e16a1417058c38f3ed22996fbd789a0bf208d3ea
frostornge <frostornge@gmail.com>
2021-07-21 23:54:04 +0900

    feat(facade/interface): add exchange rate feeder interface

--- src/facade/interface/ether/index.ts
+++ src/facade/interface/ether/index.ts
@@ -1,5 +1,6 @@
+import * as IExchangeRateFeeder from './IExchangeRateFeeder.json';
 import * as IConversionPool from './IConversionPool.json';
 import * as IRouterV2 from './IRouterV2.json';
 import * as IERC20 from './IERC20.json';
 
-export { IConversionPool, IRouterV2, IERC20 };
+export { IExchangeRateFeeder, IConversionPool, IRouterV2, IERC20 };
