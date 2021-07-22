720bf9d9e79f2882e3e8ba4bdc9786896ae1a56e
frostornge <frostornge@gmail.com>
2021-07-22 11:10:34 +0900

    env(config): include tests dir

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
 };
